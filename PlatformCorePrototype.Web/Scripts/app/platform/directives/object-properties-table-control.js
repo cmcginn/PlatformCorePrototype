316d0c9bfea5767accfa62615d530c5a87c168cb
cmcginn <Urbana@000!0>
2015-10-12 09:18:48 -0400

    Changes to object properties table control

--- PlatformCorePrototype.Web/Scripts/app/platform/directives/object-properties-table-control.js
+++ PlatformCorePrototype.Web/Scripts/app/platform/directives/object-properties-table-control.js
@@ -1,25 +1,31 @@
 ﻿myApp.directive('objectPropertiesTableControl', ['CUSTOM_APP_PROPERTIES','$log',function(constants,$log) {
-
+    function generateTable(scope) {
+        
+        scope.keys = [];
+        scope.items = [];
+        if (scope.data.length > 0) {
+            scope.keys = Object.keys(scope.data[0]);
+            scope.items = [];
+            angular.forEach(scope.data, function (item) {
+                var dataItem = [];
+                angular.forEach(Object.keys(item), function (key) {
+                    dataItem.push({ key: key, value: item[key] });
+                });
+                scope.items.push(dataItem);
+            });
+        }
+    }
     function link(scope, element, attrs) {
         $log.debug('linking objectPropertiesTableControl');
         $log.debug(scope);
+        
         scope.$on('viewDefinitionDataReceived', function(e, a) {
             $log.debug('objectPropertiesTableControl viewDefinitionDataReceived');
-            var data = a.data;
-            if (data.length > 0) {
-      
-                scope.keys = Object.keys(data[0]);
-              
-                scope.items = [];
-                angular.forEach(data, function(item) {
-                    var dataItem = [];
-                    angular.forEach(Object.keys(item), function(key) {
-                        dataItem.push({ key: key, value: item[key] });
-                    });
-                    scope.items.push(dataItem);
-
-                });
-            }
+   
+            if (a.viewId != scope.viewId)
+                return;
+            scope.data = a.data;
+            generateTable(scope);
         });
     }
 
