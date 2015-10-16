9b8e6f50e757c127ad7eef1c7f3286e7c5510d62
cmcginn <Urbana@000!0>
2015-10-16 10:02:48 -0400

    Linked List Query Strategy RunQueryTest passing

--- PlatformCorePrototype.Services/DataStructures/MongoLinkedListQueryStrategy.cs
+++ PlatformCorePrototype.Services/DataStructures/MongoLinkedListQueryStrategy.cs
@@ -40,6 +40,69 @@ namespace PlatformCorePrototype.Services.DataStructures
 
             return result;
         }
+
+        protected List<BsonDocument> GetMapFilterPipeline()
+        {
+            var result = new List<BsonDocument>(); 
+            var matchDefinition = GetMapFilterDefinition();
+            if (matchDefinition != null)
+            {
+                result.Add(new BsonDocument { { "$match", ToDocument(matchDefinition) } });
+            }
+            result.Add(new BsonDocument {{"$project", BsonDocument.Parse("{ \"$project\" : { \"_id\":0,\"Key\" : \"$Key\" } }]")}});
+            return result;
+        }
+
+
+        IMongoDatabase GetDatabase()
+        {
+            var client = new MongoClient(DataSourceLocation);
+            var result = client.GetDatabase(DataSourceName);
+            return result;
+        }
+
+        protected async Task<List<LinkedListMap<V>>> GetLinkListMaps()
+        {
+            var pipeline = new List<BsonDocument> {GetMapFilterPipeline().First()};
+            var db = GetDatabase();
+            var collection = db.GetCollection<BsonDocument>(LinkedListSettings.MapCollectionName);
+
+            var asyncResult = collection.AggregateAsync<BsonDocument>(pipeline);
+            var result = asyncResult.ContinueWith<List<LinkedListMap<V>>>((t) =>
+            {
+                var asyncTaskResult = new List<LinkedListMap<V>>();
+                var f = t.Result.ForEachAsync(doc =>
+                {
+                    var item = new LinkedListMap<V>();
+                    BsonValue bsonValue = null;
+                    if (doc.TryGetValue("Key", out bsonValue))
+                    {
+                        BsonValue bsonArray = null;
+                        if (doc.TryGetValue("Navigation", out bsonArray))
+                        {
+                            bsonArray.AsBsonArray.ToList().ForEach(s =>
+                            {
+                                item.Navigation.Add(s.ToString());
+                            });
+                            if (typeof (V) == typeof (int))
+                                item.Key = (V) (object) int.Parse(bsonValue.ToString());
+                            else if(typeof(V)==typeof(double))
+                                item.Key = (V)(object)double.Parse(bsonValue.ToString());
+                            else if (typeof (V) == typeof (Guid))
+                                item.Key = (V)(object) Guid.Parse(bsonValue.ToString());
+                            else
+                                item.Key = (V)(object)bsonValue.ToString();
+                            asyncTaskResult.Add(item);
+                        }
+                        
+
+                    }
+                    
+                });
+                return asyncTaskResult;
+            });
+            return await result;
+        }
         //TODO: Base Class
         protected BsonDocument ToDocument(FilterDefinition<T> source)
         {
@@ -93,6 +156,33 @@ namespace PlatformCorePrototype.Services.DataStructures
         //}
         public bool IncludeChildren { get; set; }
 
+        protected FilterDefinition<T> GetListFilterDefinition()
+        {
+            var builder = new FilterDefinitionBuilder<T>();
+            FilterDefinition<T> result = null;
+            var map = GetLinkListMaps().Result;
+            var ids = new BsonArray();
+            map.ForEach(linkedListMap =>
+            {
+                BsonValue val = null;
+                if (typeof (V) == typeof (int))
+                    val = new BsonInt32(int.Parse(linkedListMap.Key.ToString()));
+                ids.Add(val);
+      
+            });
+
+            result = builder.In(LinkedListSettings.KeyColumn.ColumnName, ids);
+            return result;
+        }
+
+        protected BsonDocument GetListMatchDocument()
+        {
+            BsonDocument result = null;
+            var filterDefinition = GetListFilterDefinition();
+            if(filterDefinition != null)
+                result = new BsonDocument {{"$match", ToDocument(filterDefinition)}};
+            return result;
+        }
         public LinkedListMap<V> LinkedListMap { get; set; }
 
         private List<string> _Path;
@@ -119,9 +209,17 @@ namespace PlatformCorePrototype.Services.DataStructures
             }
         }
 
-        public Task<List<T>> RunQuery()
+        public async Task<List<T>> RunQuery()
         {
-            throw new NotImplementedException();
+            var pl = new List<BsonDocument>();
+            var matchDocument = GetListMatchDocument();
+            if (matchDocument != null)
+                pl.Add(matchDocument);
+            var db = GetDatabase();
+            var collection = db.GetCollection<T>(CollectionName);
+            var asyncResult = collection.AggregateAsync<T>(pl);
+            var result = asyncResult.Result.ToListAsync<T>();
+            return await result;
         }
 
 
--- PlatformCorePrototype.Tests/DataStructures/MongoLinkedListQueryStrategyTests.cs
+++ PlatformCorePrototype.Tests/DataStructures/MongoLinkedListQueryStrategyTests.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MongoDB.Bson;
+using MongoDB.Driver;
 using PlatformCorePrototype.Core;
 using PlatformCorePrototype.Core.DataStructures;
 using PlatformCorePrototype.Services.DataStructures;
@@ -17,6 +20,23 @@ namespace PlatformCorePrototype.Tests.DataStructures
             var doc = this.GetMapFilterDefinition();
             return this.ToDocument(doc);
         }
+
+        public List<BsonDocument> GetMapFilterPipelineDocuments()
+        {
+            var docs = this.GetMapFilterPipeline();
+            return docs;
+        }
+
+        public async Task<List<LinkedListMap<int>>> GetLinkListMapsAccessor()
+        {
+            return await this.GetLinkListMaps();
+        }
+
+        public BsonDocument GetListFilterDefinitionDocument()
+        {
+            return this.GetListMatchDocument();
+     
+        }
     }
     [TestClass]
     public class MongoLinkedListQueryStrategyTests:ServiceTestBase
@@ -27,7 +47,8 @@ namespace PlatformCorePrototype.Tests.DataStructures
             {
                 CollectionName = "linkedlistdata",
                 DataSourceName = "prototype",
-                DataSourceLocation = Globals.MongoConnectionString
+                DataSourceLocation = Globals.MongoConnectionString,
+                LinkedListSettings = new LinkedListSettings {  KeyColumn=new DataColumnMetadata{ ColumnName="Account", DataType=Globals.IntegerDataTypeName}, MapCollectionName="linkedlistmap"}
       
             };
             return result;
@@ -65,6 +86,78 @@ namespace PlatformCorePrototype.Tests.DataStructures
             var actual = target.GetMapFilterDefinitionDocument().ToString();
             var expected = "{ \"Key\" : 1000 }";
             Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void GetMapFilterDefinitionDocument()
+        {
+            var target = GetAccessor();
+            target.LinkedListMap = new LinkedListMap<int>
+            {
+                Key = 1000,
+                Navigation = new List<string> { "Account", "Product", "SalesPerson" }
+            };
+            var actual = target.GetMapFilterDefinitionDocument().ToString();
+            var expected = "{ \"Navigation\" : [\"Account\", \"Product\", \"SalesPerson\"], \"Key\" : 1000 }";
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void GetMapFilterPipelineTest()
+        {
+             var target = GetAccessor();
+            target.LinkedListMap = new LinkedListMap<int>
+            {
+                Key = 1000,
+                Navigation = new List<string> { "Account", "Product", "SalesPerson" }
+            };
+            var actual = target.GetMapFilterPipelineDocuments();
+            var match = actual.First().ToString();
+            var project = actual.ElementAt(1).ToString();
+            var expected =
+                "{ \"$match\" : { \"Navigation\" : [\"Account\", \"Product\", \"SalesPerson\"], \"Key\" : 1000 } }";
+            Assert.AreEqual(expected, match, "Match documents not equal");
+            expected = "{ \"$project\" : { \"$project\" : { \"_id\" : 0, \"Key\" : \"$Key\" } } }";
+            Assert.AreEqual(expected, project, "Projection documents not equal");
+
+        }
+        [TestMethod]
+        public void GetLinkListMapsTest()
+        {
+            var target = GetAccessor();
+            target.LinkedListMap = new LinkedListMap<int>
+            {
+
+                Navigation = new List<string> { "Account", "SalesPerson" }
+            };
+            var actual = target.GetLinkListMapsAccessor().Result;
+            Assert.IsTrue(actual.Any());
+
+      
+        }
+
+        [TestMethod]
+        public void GetListFilterDefinitionDocument()
+        {
+            var target = GetAccessor();
+            target.LinkedListMap = new LinkedListMap<int>
+            {
+                Navigation = new List<string> { "Account", "SalesPerson" }
+            };
+            var actual = target.GetListFilterDefinitionDocument().ToString();
+            var expected = "{ \"$match\" : { \"Account\" : { \"$in\" : [1001, 1002, 1003, 1004, 1005] } } }";
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void RunQueryTest()
+        {
+            var target = GetAccessor();
+            target.LinkedListMap = new LinkedListMap<int>
+            {
+                Navigation = new List<string> { "Account", "SalesPerson" }
+            };
+            var actual = target.RunQuery().Result;
+            Assert.IsTrue(actual.Any());
         }
     }
 }
